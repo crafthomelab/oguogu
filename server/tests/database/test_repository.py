df92bea80d1055f515fce46056b2fb1cd3a85cba
craftsangjae <craftsangjae@gmail.com>
2025-01-14 23:35:17 +0900

    fix wrong testcodes

--- server/tests/database/test_repository.py
+++ server/tests/database/test_repository.py
@@ -13,26 +13,23 @@ async def test_create_challenge(challenge_repository: ChallengeRepository, user0
         reward_amount=100,
         title="test",
         type="photos",
-        description="test",
+        start_date=datetime.now(pytz.utc),
         end_date=datetime.now(pytz.utc) + timedelta(days=1),
         minimum_proof_count=1,
-        receipent_address=user0_account.address,
     )
     
     await challenge_repository.create_challenge(challenge)
     
-    challenge = await challenge_repository.get_challenge(challenge.hash)
-    assert challenge is not None
-    assert challenge.hash == challenge.hash
-    assert challenge.status == challenge.status
-    assert challenge.challenger_address == challenge.challenger_address
-    assert challenge.reward_amount == challenge.reward_amount
-    assert challenge.title == challenge.title
-    assert challenge.type == challenge.type
-    assert challenge.description == challenge.description
-    assert challenge.end_date == challenge.end_date
-    assert challenge.minimum_proof_count == challenge.minimum_proof_count
-    assert challenge.receipent_address == challenge.receipent_address
+    result = await challenge_repository.get_challenge(challenge.hash)
+    assert result is not None
+    assert result.hash == challenge.hash
+    assert result.status == challenge.status
+    assert result.challenger_address == challenge.challenger_address
+    assert result.reward_amount == challenge.reward_amount
+    assert result.title == challenge.title
+    assert result.type == challenge.type
+    assert result.end_date == challenge.end_date
+    assert result.minimum_proof_count == challenge.minimum_proof_count
     
 
 @pytest.mark.asyncio(loop_scope="session")
@@ -42,10 +39,9 @@ async def test_submit_proofs(challenge_repository: ChallengeRepository, user0_ac
         reward_amount=100,
         title="test",
         type="photos",
-        description="test",
+        start_date=datetime.now(pytz.utc),
         end_date=datetime.now(pytz.utc) + timedelta(days=1),
         minimum_proof_count=2,
-        receipent_address=user0_account.address,
     )
     
     await challenge_repository.create_challenge(challenge)
